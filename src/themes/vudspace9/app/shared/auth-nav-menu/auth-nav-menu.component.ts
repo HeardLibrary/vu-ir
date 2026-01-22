38887121ee291a4b57ac51f351117d4a5b418f2b
Dale Poulter <poulterd@gmail.com>
2026-01-22 10:49:35 -0600

    add Home button,  modified narbar to remove duplicates and added an About stub

--- src/themes/vudspace9/app/shared/auth-nav-menu/auth-nav-menu.component.ts
+++ src/themes/vudspace9/app/shared/auth-nav-menu/auth-nav-menu.component.ts
@@ -21,8 +21,8 @@ import { BrowserOnlyPipe } from '../../../../../app/shared/utils/browser-only.pi
 
 @Component({
   selector: 'ds-themed-auth-nav-menu',
-  // templateUrl: './auth-nav-menu.component.html',
-  templateUrl: '../../../../../app/shared/auth-nav-menu/auth-nav-menu.component.html',
+  templateUrl: './auth-nav-menu.component.html',
+  // templateUrl: '../../../../../app/shared/auth-nav-menu/auth-nav-menu.component.html',
   // styleUrls: ['./auth-nav-menu.component.scss'],
   styleUrls: ['../../../../../app/shared/auth-nav-menu/auth-nav-menu.component.scss'],
   animations: [fadeInOut, fadeOut],
